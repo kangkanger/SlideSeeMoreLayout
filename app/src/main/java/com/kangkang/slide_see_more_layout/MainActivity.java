8ff948e956248410969d392451180a7a2660aa05
kxx <codekxx@163.com>
2018-12-10 17:19:56 +0800

    Initial commit

---
+++ app/src/main/java/com/kangkang/slide_see_more_layout/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kangkang.slide_see_more_layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
